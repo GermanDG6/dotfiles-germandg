6473970572334ac74f31ff20d4d3e46d4718cd84
German Delgado <germandg6@gmail.com>
2022-12-12 12:34:28 +0100

    first commit

---
+++ os/install.sh
@@ -0,0 +1,9 @@
+# Install oh my zsh
+
+if [[ ! -d ~/.oh-my-zsh ]]; then
+
+    export RUNZSH=no
+
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+fi
