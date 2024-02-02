5b3791f9001eb93ec5b8c5a11ac8bdc3b875bdae
German Delgado <germandg6@gmail.com>
2024-02-02 14:26:20 +0000

    feat: add aliases for console

---
+++ zsh/aliases-germandg.zsh
@@ -0,0 +1,5 @@
+## PATHS
+alias w="cd $HOME/workspace"
+
+# Chrome CORS disabled
+alias ccors="open -n -a /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --args --user-data-dir=\"/tmp/chrome_dev_test\" --disable-web-security"
