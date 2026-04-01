28e1abd15d00383768268ea8f1a6f6427ff0e093
GermanDG6 <germandg6@gmail.com>
2026-04-01 12:53:56 +0100

    feat: add macOS configuration settings for system preferences and keyboard

--- os/install.sh
+++ os/install.sh
@@ -7,3 +7,6 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 fi
+
+# Change MacOS configuration
+source $HOME/.dotfiles/plugins/dotfiles-germandg/os/.macos
